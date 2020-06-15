bd6c925a4707cc2e5252b86c20422367af14647d
iamsuigeneris <irewolelanre@gmail.com>
2020-06-15 23:10:54 +0100

    amazing colour board

---
+++ colour-board/color.js
@@ -0,0 +1,38 @@
+/*
+   FIRST METHOD
+*/
+// const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B','C','D','E','F']
+// const board = document.querySelector('.main-board');
+// const colorName = document.getElementById('current-color')
+// const btn = document.getElementById('btn')
+
+// btn.addEventListener('click', function(){
+//     let randomColor = '#';
+//     for(let i = 0;i < 6;i++){
+//         randomColor += hex[getRandomColor()];
+//     }
+//     board.style.backgroundColor = randomColor;
+//     colorName.textContent = randomColor;
+// })
+
+// function getRandomColor(){
+//     return Math.floor(Math.random() * hex.length)
+// }
+/*
+    SECOND METHOD
+*/
+
+const board = document.querySelector('.main-board');
+const colorName = document.getElementById('current-color')
+const btn = document.getElementById('btn')
+
+btn.addEventListener('click',function(){
+    board.style.backgroundColor = getRandomColor();
+    colorName.textContent = getRandomColor();
+  
+})
+
+function getRandomColor(){
+    return '#' + Math.random().toString(16).substring(2,8).toUpperCase();
+}
+
